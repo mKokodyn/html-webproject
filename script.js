86bd79293c1b57fc3864339e1124c0a6017e3bc4
mKokodyn <kokodynskyy@gmail.com>
2025-02-10 00:21:08 +0000

    fix

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let offset = 0;
 const sliderLine = document.querySelector(".our-team__slider__line");
 let btnNext = document.querySelector(".btn-next");
 let btnPrev = document.querySelector(".btn-prev");
+const slider = document.querySelector(".our-team__slider");
 let slidesOnPage = 0;
 
 btnNext.addEventListener('click', NextSlide);
@@ -9,6 +10,8 @@ btnPrev.addEventListener('click', PreviousSlide);
 window.addEventListener('resize', getSlidesToShow);
 window.addEventListener('resize', reloadCarousel);
 window.addEventListener('load', getSlidesToShow);
+window.addEventListener("load", positionButtons);
+window.addEventListener("resize", positionButtons);
 
 function NextSlide() {
 
@@ -58,3 +61,36 @@ function reloadCarousel() {
     }
 
 }
+
+
+function positionButtons() {
+    
+
+    if (!slider || !btnPrev || !btnNext) return;
+
+    // Отримуємо висоту каруселі
+    const sliderHeight = slider.offsetTop;
+
+    // Встановлюємо кнопки по центру каруселі
+    btnPrev.style.top = `${sliderHeight + slider.offsetHeight/2}px`;
+    
+    let paddingRight = parseInt(window.getComputedStyle(document.querySelector('.carousel__image')).getPropertyValue('padding-right'));
+    
+
+    let buttonNextOffset = btnNext.clientWidth/2 + paddingRight;
+    let buttonPrevOffset = btnNext.clientWidth/2 - paddingRight;
+
+    btnNext.style.top = `${sliderHeight + slider.offsetHeight/2}px`;
+    btnNext.style.left = `${slider.offsetWidth + slider.offsetLeft - buttonNextOffset}px`;
+    btnPrev.style.left = `${slider.offsetWidth + slider.offsetLeft - 256 - buttonPrevOffset}px`;
+    
+    if (window.innerWidth >= 768){
+        btnPrev.style.left = `${slider.offsetWidth + slider.offsetLeft - 512 - buttonPrevOffset}px`;
+    }
+    if (window.innerWidth >= 1024){
+        btnPrev.style.left = `${slider.offsetWidth + slider.offsetLeft - 768 - buttonPrevOffset}px`;
+    }
+}
+
+
+
